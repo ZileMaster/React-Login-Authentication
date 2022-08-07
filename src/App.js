745cbefd839b79a9a94cac33dcb54fa63660f049
ZileMaster <azivkovic10120ri@raf.rs>
2022-08-07 16:16:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import LoginButton from './components/loginButton';
 import './App.css';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if(isLoading) return <div> Loading ...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginButton/>
+      <LogoutButton/>
+      <Profile />
+    </>
   );
 }
 
---
+++ src/components/LogoutButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LogoutButton = () => {
+    const {logout, isAuthenticated} = useAuth0(); 
+
+    return (
+        isAuthenticated && (
+            <button onClick={() => logout()}> log out </button>
+        )
+    )
+}
+
+export default LogoutButton
